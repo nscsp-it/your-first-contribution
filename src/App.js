bf0d5bbf2ad4c31daddb8783fa913e1c0c50b6b3
t-rhex <andrewadhikari2016@gmail.com>
2021-09-28 09:23:46 -0500

    New changes in the table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from './Table';
+import { DataGridDemo } from './Table';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to your-first-contribution Github Page 👋 </h1>
-        <Table></Table>
+        <DataGridDemo></DataGridDemo>
       </div>
     );
   }
--- src/Table.js
+++ src/Table.js
@@ -1,32 +1,64 @@
-import React from 'react'
-import MaterialTable from 'material-table'
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 
-export const Table=()=>{
-    const data = [
-        {name: 'Andrew', fc: 'yes'},
-        {name: 'Johny', fc: 'no'}
-        
-    ]
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'country',
+    headerName: 'Country',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'ftc',
+    headerName: 'First time contributor ? ',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue(params.id, 'firstName') || ''} ${
+        params.getValue(params.id, 'lastName') || ''
+      }`,
+  },
+];
 
-    const columns=[
-        {
-            title: 'Name', field: 'name'
-        },
-        {
-            title: 'First contirbution ? ' , field: 'fc'
-        }
-    ]
-    return <div>
-        <MaterialTable title=""
-        
-        data = {data}
+const rows = [
+  { id: 1, lastName: 'Adhikari', firstName: 'Andrew',country: 'United States', ftc: 'no' }
+ 
+];
+
+export const DataGridDemo=()=> {
+  return (
+   <div className = "centered"> 
+         <div style={{ height: 800 , width: '100%'}}>
+      <DataGrid
+        rows={rows}
         columns={columns}
-        options={
-            {search:false,
-            paging: false
-            }
-        }
-
-        />
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
     </div>
+   </div>
+  
+  );
 }
