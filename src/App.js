2037987bdac10dc7a067f48ba80ce79e2ffc3f9e
NSCSP - IT Connexion <91425211+nscsp-it@users.noreply.github.com>
2021-09-27 21:22:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to LOADDDDDD
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Session Tueday @ 7 pm . 
-     
+       <h1> My name is Andrew </h1>
         </a>
       </header>
     </div>
