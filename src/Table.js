f280a7ea3ceb4e319236bff091da658053c75bc0
josepbaidya <baidya.josep@gmail.com>
2021-10-15 12:55:22 -0500

    removed Hello Word Row

--- src/Table.js
+++ src/Table.js
@@ -44,7 +44,6 @@ const columns = [
 const rows = [
   { id: uuidv4(), lastName: 'Adhikari', firstName: 'Andrew',country: 'United States', ftc: 'no' },
   { id: uuidv4(), lastName: 'Baidya', firstName: 'Josep',country: 'United States', ftc: 'yes' },
-  { id: uuidv4(), lastName: 'World', firstName: 'Hello',country: 'United States', ftc: 'yes' },
   { id: uuidv4(), lastName: 'Thapa Magar', firstName: 'Aashish',country: 'United States', ftc: 'yes' }
 ];
 
